2ca81d4ee0da1570755c8f3f09d29491404aa1af
Rodrigo Militao <militaorodrigo9@gmail.com>
2020-10-13 15:43:53 -0300

    Updates

---
+++ precache-manifest.dde798d65a9189347638683363ba41db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9a70a24b028996e236aab9a6266cc9",
+    "url": "/my-advice/index.html"
+  },
+  {
+    "revision": "7d59c73654616310e9c8",
+    "url": "/my-advice/static/css/main.31c08b09.chunk.css"
+  },
+  {
+    "revision": "d8a59f5250465de9114b",
+    "url": "/my-advice/static/js/2.e3ca4e5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-advice/static/js/2.e3ca4e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d59c73654616310e9c8",
+    "url": "/my-advice/static/js/main.7205cef1.chunk.js"
+  },
+  {
+    "revision": "dbb7560bee9f3a5d12e5",
+    "url": "/my-advice/static/js/runtime-main.89d4853d.js"
+  },
+  {
+    "revision": "9a0085b31b9af137d80e0fdc3f6539e1",
+    "url": "/my-advice/static/media/logo.9a0085b3.png"
+  }
+]);
